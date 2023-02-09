3a7403adb6b8bb5e485f9dd4ba9909f83c13ad2a
Marc Norris <marcnorris007@icloud.com>
2023-02-09 19:47:03 +0000

    data appended to modals

--- assets/script.js
+++ assets/script.js
@@ -246,30 +246,115 @@ $("#searchsubmit").on("click", function (event) {
 
 
 function getApiData(recipeName) {
-    
-    fetch(`https://api.spoonacular.com/recipes/complexSearch?query=${recipeName}&addRecipeInformation=true&number=${NumberOfRecipe}&apiKey=${marcNewApi}`)
-    
-    .then(response => response.json())
-    .then(data =>  {
-        
-        let recipes = data.results;
-        let html = "";
-        console.log(recipes);
 
-           for (let i = 0; i < recipes.length; i++) {
-            let recipe = recipes[i];
+    fetch(`https://api.spoonacular.com/recipes/complexSearch?query=${recipeName}&addRecipeInformation=true&number=${NumberOfRecipe}&apiKey=${robApi}`)
 
-            let title = recipe.title;
-            console.log(title);
+        .then(response => response.json())
+        .then(data => {
 
-            let imageURL = recipe.image;
-            console.log(imageURL);
+            let recipes = data.results;
+            let cardData = document.getElementById('slider');
+            let modalData = document.getElementById('modals');
+            let html = "";
+            let modalHtml = "";
+            console.log(recipes);
 
-            let summary = recipe.summary;
-            console.log(summary);
+            for (let i = 0; i < recipes.length; i++) {
+                let recipe = recipes[i];
+
+                let title = recipe.title;
+                console.log(title);
+
+                let imageURL = recipe.image;
+                console.log(imageURL);
+
+                let summary = recipe.summary;
+                console.log(summary);
+
+
+                html += `
+            <li>
+                    <div class="uk-card uk-card-default">
+                        <div class="uk-card-media-top" id="recipe-image-1">
+                            <img src="${imageURL}"
+                                alt="" class="wdn-stretch rounded-corners">
+                        </div>
+                        <div class="uk-card-body">
+                            <h3 class="uk-card-title-2">${title}</h3>
+                            <!-- This button launches the modal -->
+                            <div class="recipe-button" id="recipe-button-2">
+                                <button type="button" class="btn btn-info" data-toggle="modal" data-target="#modal${i}">
+                                    Recipe
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                </li>
+            `
+
+                modalHtml += `
+            <div class="modal fade" id="modal${i}" role="dialog" aria-labelledby="exampleModalLongTitle"
+        aria-hidden="true">
+        <div class="modal-dialog modal-lg" role="document">
+            <div class="modal-content">
+                <div class="modal-header" id="modal-header">
+                    <h3 class="modal-title" id="exampleModalLongTitle">${title}</h3>
+                    <div id="emojiContainer" class="emoji-container">
+                        <!-- Emoji from API goes here -->
+                    </div>
+                </div>
+                <div class="modal-body">
+                    <div class="modal-top" id="modal-recipe-image-1">
+                        <img src="${imageURL}"
+                            class="card-img-top modal-image" alt="...">
+
+                    </div>
+                    <div class="modal-bottom">
+                        <h4 class="recipe">
+                            Summary
+                        </h4>
+                        <p class="recipe-text">
+                            ${summary}
+                        </p>
+                        <a href="http://fullbellysisters.blogspot.com/2012/06/pasta-with-garlic-scallions-cauliflower.html"
+                            target="_blank">read more<i class="fa-solid fa-angle-right"></i></a>
+                    </div>
+                </div>
+
+                <div class="modal-footer">
+                    <div class="like-dislike left-buttons">
+                        <!-- Like counter will increment by 1 and only allow 1 click per uesr -->
+                        <button id="like-counter-${i}" type="button" class="btn btn-success"><i
+                                class="fa-solid fa-thumbs-up"></i>Like</button>
+                        <!-- Dislike counter will increment by 1 and only allow 1 click per uesr -->
+                        <button id="dislike-counter-${i}" type="button" class="btn btn-danger"><i
+                                class="fa-solid fa-thumbs-down"></i>Dislike</button>
+                        <a><i id="clear-buttons-${i}" class="fa-regular fa-circle-xmark"></i></a>
+                    </div>
+                    <button type="button" id="clear-button" class="btn btn-light" data-dismiss="modal">Close</button>
+                </div>
+
+            </div>
+        </div>
+    </div>
             
-           }
+            `
+
+                
+
+            }
+
+            cardData.innerHTML = html
+            modalData.innerHTML = modalHtml
+
+            for (let i = 0; i < recipes.length; i++) {
+                emojiCode(i);
+            }
+
         });
+
+        
+
 };
 
 getApiData();
@@ -357,68 +442,74 @@ getApiData();
 
 
 
+function emojiCode(index) {
+    let likeBtn = document.querySelector(`#like-counter-${index}`);
+    let clearLikeBtn = document.querySelector(`#clear-buttons-${index}`);
+    let clearDislikeBtn = document.querySelector(`#clear-buttons-${index}`);
+    let dislikeBtn = document.querySelector(`#dislike-counter-${index}`);
 
-let likeBtn = document.querySelector("#like-counter");
-let clearLikeBtn = document.querySelector("#clear-buttons");
-let clearDislikeBtn = document.querySelector("#clear-buttons");
-let dislikeBtn = document.querySelector('#dislike-counter');
+    console.log(likeBtn, index)
+    console.log(dislikeBtn, index)
 
-function likeDislike() {
+    function likeDislike() {
 
-    let emojiContainer = document.getElementById('emojiContainer');
+        let emojiContainer = document.getElementById('emojiContainer');
 
-    fetch("https://emoji-api.com/emojis?access_key=5715a029ed3fd2e22383b01663524c9319a127f7")
-        .then(response => response.json())
-        .then(function (response) {
-            let emojis = response;
-            // console.log(emojis)
-            let thumbsDownEmoji = (response[186].character)
-            let heartEmoji = (response[185].character)
+        fetch("https://emoji-api.com/emojis?access_key=5715a029ed3fd2e22383b01663524c9319a127f7")
+            .then(response => response.json())
+            .then(function (response) {
+                let emojis = response;
+                // console.log(emojis)
+                let thumbsDownEmoji = (response[186].character)
+                let heartEmoji = (response[185].character)
 
-            likeBtn.addEventListener('click', function () {
+                likeBtn.addEventListener('click', function () {
 
-                let likeEmoji = document.createElement('h4');
-                likeEmoji.setAttribute('id', 'para-1');
+                    let likeEmoji = document.createElement('h4');
+                    likeEmoji.setAttribute('id', 'para-1');
 
-                likeEmoji.innerHTML = `${heartEmoji}`
-                emojiContainer.append(likeEmoji);
-                dislikeBtn.disabled = true
-                likeBtn.disabled = true
+                    likeEmoji.innerHTML = `${heartEmoji}`
+                    emojiContainer.append(likeEmoji);
+                    dislikeBtn.disabled = true
+                    likeBtn.disabled = true
 
-                clearLikeBtn.addEventListener('click', function () {
+                    clearLikeBtn.addEventListener('click', function () {
 
-                    likeBtn.disabled = false
-                    dislikeBtn.disabled = false
-                    likeEmoji.remove();
+                        likeBtn.disabled = false
+                        dislikeBtn.disabled = false
+                        likeEmoji.remove();
 
-                })
+                    })
+
+                });
+
+                dislikeBtn.addEventListener('click', function () {
+
+                    let dislikeEmoji = document.createElement('h4');
+                    dislikeEmoji.setAttribute('id', 'para-2');
+
+                    dislikeEmoji.innerHTML = `${thumbsDownEmoji}`
+                    emojiContainer.append(dislikeEmoji);
+                    likeBtn.disabled = true
+                    dislikeBtn.disabled = true
+
+                    clearDislikeBtn.addEventListener('click', function () {
+
+                        likeBtn.disabled = false
+                        dislikeBtn.disabled = false
+                        dislikeEmoji.remove();
+
+                    })
+                });
 
             });
 
-            dislikeBtn.addEventListener('click', function () {
+    };
 
-                let dislikeEmoji = document.createElement('h4');
-                dislikeEmoji.setAttribute('id', 'para-2');
+    likeDislike();
+}
 
-                dislikeEmoji.innerHTML = `${thumbsDownEmoji}`
-                emojiContainer.append(dislikeEmoji);
-                likeBtn.disabled = true
-                dislikeBtn.disabled = true
 
-                clearDislikeBtn.addEventListener('click', function () {
-
-                    likeBtn.disabled = false
-                    dislikeBtn.disabled = false
-                    dislikeEmoji.remove();
-
-                })
-            });
-
-        });
-
-};
-
-likeDislike();
 
 
 
